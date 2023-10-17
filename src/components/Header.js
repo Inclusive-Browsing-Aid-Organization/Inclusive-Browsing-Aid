1e8c5bce9268a3caa722ccf5c51eaf97a3761816
Nathaniel Klump <nathanieljklump@gmail.com>
2023-10-16 22:02:37 -0400

    style menu 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <h1 className="text-2xl font-bold text-ultraViolet m-4">Inclusive Browsing Aid</h1>
+    <h1 className="text-2xl font-bold text-ultraViolet mb-4">Inclusive Browsing Aid</h1>
   );
 };
 
